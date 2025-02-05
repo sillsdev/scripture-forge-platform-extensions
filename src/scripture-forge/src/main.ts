28a198d3c5f50e25f7e472bb1e07fc0b28599306
tjcouch-sil <tj_couch@sil.org>
2025-02-05 12:23:15 -0600

    Customized scripture-forge extension details

--- src/scripture-forge/src/main.ts
+++ src/scripture-forge/src/main.ts
@@ -1,10 +1,10 @@
 import { logger } from '@papi/backend';
 
 export async function activate() {
-  logger.info('Extension template is activating!');
+  logger.info('Scripture Forge Extension is activating!');
 }
 
 export async function deactivate() {
-  logger.info('Extension template is deactivating!');
+  logger.info('Scripture Forge Extension is deactivating!');
   return true;
 }
